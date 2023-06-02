63fb0345f1beca13b683de548c874c5fd97e53c3
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2023-06-02 11:15:11 -0600

    IndexPage return logic adjustment

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,30 +1,25 @@
 import { HeadContainer, HeaderContainer, MainContainer, FooterContainer } from "../containers"
 import { Loading } from "../components"
 import { useState, useEffect } from "react";
-import tw from "twin.macro";
 
 const IndexPage = () => {
-  const [loading, setLoading] = useState(true)
-  useEffect(() => setLoading(false), [])
+	const [loading, setLoading] = useState(true)
+	useEffect(() => setLoading(false), [])
 
-  return (
-    <>
-      {!loading ? (
-        <>
-            <HeadContainer/>
-            <HeaderContainer/>
-            <MainContainer/>
-            <FooterContainer/>
-        </>
-      ) : (
-        <Loading>
-          <Loading.Frame>
-            <Loading.Loader tw="bg-purpleCircle! bg-cover!"/>
-          </Loading.Frame>
-        </Loading>
-      )}
-    </>
-  )
+	return loading ? (
+		<Loading>
+			<Loading.Frame>
+				<Loading.Loader />
+			</Loading.Frame>
+		</Loading>
+	) : (
+		<>
+			<HeadContainer/>
+			<HeaderContainer/>
+			<MainContainer/>
+			<FooterContainer/>
+		</>
+	)
 }
 
 export default IndexPage
